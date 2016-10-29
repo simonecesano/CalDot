4a9c5f9be265659d9ec351e1699329204a629333
Simone Cesano <simonecesano@libero.it>
2016-10-29 10:20:26 +0200

    Cleaned up cruft

--- public/js/textwrap.js
+++ public/js/textwrap.js
@@ -53,183 +53,33 @@ function getRoot(el){
     }
 }
 
-function layout_01(nodes) {
-    nodes.each(function(e, i) { e.rect = this.getBoundingClientRect() });
-    nodes = nodes.sort(function(a, b){
-	// if (a.rect.y != b.rect.y) return a.rect.y - b.rect.y
-	// return Math.random() - 0.5;
-	return a.rect.x != b.rect.x ? a.rect.x == b.rect.x : a.rect.y - b.rect.y
-	// return a.task < b.task ? -1 : a.task > b.task;
-    })
-
-    var nodes_array = [];
-    nodes.each(function(e, i) {
-	delete e.rect;
-	console.log(i + ' ' + e.task); 
-	nodes_array.push(this) });
-    var y_pos = [];
-
-    var repeat = 1;
-    var repeats = 0;
-
-    var upper_bound = nodes_array[0].getBoundingClientRect().top;
-    
-    while (repeat > 0 && repeats < 4) {
-	repeat = 0;
-	repeats++;
-	nodes.each(function(e, i){
-	    var rect = this.getBoundingClientRect()
-
-	    for (k = i + 1; k < nodes.size(); k++) {
-		if (k == i) continue;
-		var n = nodes_array[k];
-		if (collide(rect, n.getBoundingClientRect())) {
-		    repeat = 1;
-		    var d =  d3.select(n);
-		    var r = d3.select(this)
-
-		    // console.log(r.datum().task.substr(0, 24));
-		    // console.log('> ' + d.datum().task.substr(0, 24));
-
-		    var t = rect.bottom - n.getBoundingClientRect().top;
-
-		    // push it down
-		    if (d.attr('transform')) {
-			var o = d.attr('transform')
-			d.attr('transform', o + ' translate(0,' + t + ')')
-		    } else {
-			d.attr('transform', 'translate(0,' + t + ')')
-		    }
-		}
-	    }
-	});
-    }
-}
-
-function collide(r1, r2){
-    return !(r2.left > r1.right || 
-	     r2.right < r1.left || 
-	     r2.top > r1.bottom ||
-	     r2.bottom < r1.top);
-}
-
-function layout_02(nodes) {
-    nodes.each(function(e, i) { e.rect = this.getBoundingClientRect() });
-    nodes = nodes.sort(function(a, b){
-	return a.rect.x != b.rect.x ? a.rect.x - b.rect.x : a.rect.y - b.rect.y
-	// return a.rect.x - b.rect.x;
-    })
-
-    var nodes_array = [];
-    nodes.each(function(e, i) {
-	delete e.rect;
-	console.log(i + ' ' + e.task); 
-	nodes_array.push(this)
-    });
-    var y_pos = [];
-
-    var repeat = 1;
-    var repeats = 0;
-
-    var upper_bound = nodes_array[0].getBoundingClientRect().top;
-    
-    while (repeat > 0 && repeats < 1) {
-	repeat = 0;
-	repeats++;
-	nodes.each(function(e, i){
-	    var rect = this.getBoundingClientRect()
-	    
-	    // put the rectangle at the top
-	    // if it collides with any preceding one, push it down
-	    // then take all following ones
-	    // and if they conflict with this one, push them down
-	    // take item to upper bound
-
-	    // put the rectangle at the top
-
-	    // console.log(d.attr('transform'));
-	    // console.log(this.getBoundingClientRect())
-	    
-	    rect = this.getBoundingClientRect()
-
-	    for (k = i + 1; k < nodes.size(); k++) {
-		if (i == k) {  continue }
-		var n = nodes_array[k];
-		if (collide(rect, n.getBoundingClientRect())) {
-		    var d =  d3.select(n);
-		    var t = rect.bottom - n.getBoundingClientRect().top;
-
-		    // console.log(d3.select(this).text().substring(0, 24));
-		    // console.log(d.text().substring(0, 24));
-		    // console.log('++++++++++++++++++++')
-
-		    // push it down
-		    if (d.attr('transform')) {
-			var o = d.attr('transform')
-			d.attr('transform', o + ' translate(0,' + t + ')')
-		    } else {
-			d.attr('transform', 'translate(0,' + t + ')')
-		    }
-		    // console.log(d.attr('transform'));
-		}
-	    }
-	    // console.log(d.attr('transform'));
-	    // console.log(this.getBoundingClientRect())
-	    var t = upper_bound - this.getBoundingClientRect().top;
-	    var d = d3.select(this)
-
-	    // console.log(this.getBoundingClientRect())
-
-	    var o = d.attr('transform')
-	    var transform = o ? o + ' translate(0,' + t + ')' : 'translate(0,' + t + ')'
-
-	    d.attr('transform', transform)
-	    // console.log(d.attr('transform'));
-	    // console.log(this.getBoundingClientRect())
-
-	    // if it collides with any preceding one, push it down
-
-	    for (k = 0; k < i; k++) {
-		var rect = this.getBoundingClientRect()
-		var n = nodes_array[k];
-		if (d.text().match(/art/i) || i == 11 || k == 11) {
-		    console.log(d.text().substring(0, 24));
-		    console.log(d3.select(n).text().substring(0, 24));
-		    console.log(rect)
-		    console.log(n.getBoundingClientRect())
-		    console.log(collide(rect, n.getBoundingClientRect()))
-		    console.log('---------------------')
-		}
-		if (collide(rect, n.getBoundingClientRect())) {
-		    var t = n.getBoundingClientRect().bottom - rect.top;
-		    if (d.attr('transform')) {
-			var o = d.attr('transform')
-			d.attr('transform', o + ' translate(0,' + t + ')')
-		    } else {
-			d.attr('transform', 'translate(0,' + t + ')')
-		    }
-		}		
-	    }
-	    
-	    console.log('#############################')
-	});
-    }
-}
-
 
 function layout_03(nodes) {
+    
     nodes.each(function(e, i) { e.rect = this.getBoundingClientRect() });
 
     // sort the nodes left to right
-    nodes_sorted = nodes.sort(function(a, b){ return a.rect.x != b.rect.x ? a.rect.x - b.rect.x : a.rect.y - b.rect.y })
+    var nodes_sorted = nodes.sort(function(a, b){
+	console.log(a);
+	return a.rect.x != b.rect.x ? a.rect.x - b.rect.x : a.rect.y - b.rect.y
+    })
 
     var left_point = 0; var top_point = 0; var right_limit;
-    var placed = 0; var margin = 6;
+    var placed = 0; var placed_idx = {};
+    var margin = 6;
     var nodes_array = [];
-    nodes_sorted.each(function(e, i) { delete e.rect; nodes_array.push(this) });
+    nodes_sorted.each(function(e, i) {
+	delete e.rect;
+	// console.log(this);
+	console.log(this.parentNode);
+	nodes_array.push(this);
+	placed_idx[i] = 0;
+    });
 
+
+    console.log('---------------------');
     var b_left = 0; // left boundary
-    var b_top = nodes_array[0].getBoundingClientRect().top;;
+    var b_top = nodes_array[0].getBoundingClientRect().top;
     
     // repeat until there are no nodes in the array to be placed
     while (placed < nodes_array.length) {
@@ -239,8 +89,7 @@ function layout_03(nodes) {
 	// get the first node whose left is >= than left_point and place it
 	// repeat until you have tried to place all nodes
 	while (i < nodes_array.length) {
-	    if (nodes_array[i] && nodes_array[i].getBoundingClientRect().x > b_left) {
-		// console.log(nodes_array[i].getBoundingClientRect());
+	    if (!placed_idx[i] && nodes_array[i].getBoundingClientRect().x > b_left) {
 
 		var t = b_top - nodes_array[i].getBoundingClientRect().top;
 		var d = d3.select(nodes_array[i])
@@ -253,11 +102,11 @@ function layout_03(nodes) {
 		// console.log(nodes_array[i].getBoundingClientRect());
 		b_left = nodes_array[i].getBoundingClientRect().right
 		max_height = max_height > nodes_array[i].getBoundingClientRect().height ? max_height : nodes_array[i].getBoundingClientRect().height;
-		console.log(max_height);
-		// delete the node from the array
-		nodes_array[i] = undefined;
+		// tag the node as placed
+		placed_idx[i] = 1;
 		
 		placed++;
+		console.log(nodes_array[i].parentNode);
 	    } else {
 	    }
 	    i++
