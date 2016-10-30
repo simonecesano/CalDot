4db48c433e4551a73e990d7c30cd11c715d4ede7
Simone Cesano <simonecesano@libero.it>
2016-10-30 07:52:58 +0100

    cleaned up, added help

--- caldot.pl
+++ caldot.pl
@@ -1,32 +1,10 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
-use String::Koremutake;
-use DateTime;
-
-# Documentation browser under "/perldoc"
-plugin 'PODRenderer';
-# push @{app->static->paths}, app->home->rel_dir('public');
 
 get '/' => sub {
   my $c = shift;
-  $c->render(template => 'load');
+  $c->render(template => 'index');
 };
-
-post '/' => sub {
-  my $c = shift;
-  $c->render(json => { message => 'done'});
-};
-
-
-get '/holidays' => [ format => [qw/json/]] => sub {
-    my $c = shift;
-    my $k = String::Koremutake->new;
-    my $dt = DateTime->new(year => 2016);
-    my $dates = [ map { { name => $k->integer_to_koremutake(rand(100000)), date => $dt->clone->add( days => rand(365)) } } (0..10) ];
-    $c->render(json => $dates)
-};
-
-get '/holidays';
 
 get '/load';
 
@@ -34,7 +12,12 @@ get '/chart';
 
 get '/table';
 
+get '/help/*help' => sub {
+    my $c = shift;
+    $c->render(text => $c->param('help'))
+};
 
 app->start;
+
 __DATA__
 
